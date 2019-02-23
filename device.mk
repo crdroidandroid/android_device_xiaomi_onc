97855b5d35e6a789f8401fbe9508a32bb88f2146
Offain <fabian.leutenegger@bluewin.ch>
2019-02-23 22:21:04 +0100

    onc: Bind mount unsigned kernel module
     * We cant use the stock signed kernel module
    
    Change-Id: I9530bf4aeadc0bdd58eb1519d893188e40d5c13a

--- device.mk
+++ device.mk
@@ -46,6 +46,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     Snap
 
+# Device init scripts
+PRODUCT_PACKAGES += \
+    init.target.rc
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
