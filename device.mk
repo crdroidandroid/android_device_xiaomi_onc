b1e4267aa789cd0c9ebdeb5a850ddccb8a1512bc
Offain <fabian.leutenegger@bluewin.ch>
2019-02-23 07:56:35 +0100

    onc: Add device specific overlays
     * Includes corner, power profile and brightness configs
    Change-Id: I84104b74c9e0b070fd74885ded14070029b65792
    Change-Id: Id7ef1fa6d717ddbfd913a329a4f47eb2ab893055

--- device.mk
+++ device.mk
@@ -27,6 +27,11 @@ TARGET_SCREEN_WIDTH := 720
 # Properties
 -include $(LOCAL_PATH)/device-props.mk
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
