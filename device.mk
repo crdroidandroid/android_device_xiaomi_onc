c58fda4d0ff85e23f0b564569ac80e6aa11e6feb
Jiyong Park <jiyong@google.com>
2019-09-16 08:46:06 +0530

    onc: Generate Apex Image & Add updatable_apex.mk

--- device.mk
+++ device.mk
@@ -18,6 +18,9 @@ $(call inherit-product, vendor/xiaomi/onc/onc-vendor.mk)
 $(call inherit-product, vendor/xiaomi/sdm632-common/sdm632-common-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
