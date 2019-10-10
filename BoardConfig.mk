dec1a84fb72787aacfa9a35d23faccc083ae58c1
RevanthTolety <crevanthtolety@gmail.com>
2019-10-10 11:43:18 +0530

    onc: Add Initial Sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -145,7 +145,10 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_RIL_VARIANT := caf
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/qcom/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/qcom/sepolicy/public
 
 # Sensors
 USE_SENSOR_MULTI_HAL := true
