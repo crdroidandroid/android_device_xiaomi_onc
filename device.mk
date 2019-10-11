5015b52cffd6f2d95b6dc0952028f3ff1ddb2944
RevanthTolety <crevanthtolety@gmail.com>
2019-10-11 08:23:20 +0530

    onc: Update Overlays & Dependencies

--- device.mk
+++ device.mk
@@ -31,7 +31,7 @@ TARGET_SCREEN_WIDTH := 720
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
-
+	
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
